75abe6875c54668fa40af00af45ec41d7af46696
Albert Likaj <dagobah1528@gmail.com>
2024-11-23 22:20:02 +0100

    Add flycheck_init

---
+++ flycheck_init.el
@@ -0,0 +1,25 @@
+;;; flycheck_init.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 computer
+;;
+;; Author: computer <computer@computer>
+;; Maintainer: computer <computer@computer>
+;; Created: November 10, 2024
+;; Modified: November 10, 2024
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/computer/flycheck_init
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'flycheck_init)
+;;; flycheck_init.el ends here
